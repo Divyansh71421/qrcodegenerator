f84dd52b46d2c59ae340258e83e57264ae9ba37e
Divyansh71421 <91370057+Divyansh71421@users.noreply.github.com>
2022-04-10 01:46:29 +0530

    Add files via upload
    import qrcode
    qr=qrcode.QRCode(
        version=15,
        box_size=10,
        border=5
    )
    data="https://leetcode.com/problems/two-sum/"
    qr.add_data(data)
    qr.make(fit=True)
    img=qr.make_image(fill="black",back_color="white")
    img.save('text.png')

---
+++ qrcodegenerator.py
@@ -0,0 +1,11 @@
+import qrcode
+qr=qrcode.QRCode(
+    version=15,
+    box_size=10,
+    border=5
+)
+data="https://leetcode.com/problems/two-sum/"
+qr.add_data(data)
+qr.make(fit=True)
+img=qr.make_image(fill="black",back_color="white")
+img.save('text.png')
